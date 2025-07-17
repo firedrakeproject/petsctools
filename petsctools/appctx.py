8d680b13cd4bae8ebd9741d1c0be45017beb54cf
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2025-07-17 20:51:53 +0100

    appctx

---
+++ petsctools/appctx.py
@@ -0,0 +1,28 @@
+import itertools
+
+
+class Appctx:
+    def __init__(self):
+        self._count = itertools.count()
+        self._data = {}
+        self._missing_key = next(self._count)
+
+    @property
+    def missing_key(self):
+        return self._missing_key
+
+    def insert(self, val):
+        key = next(self._count)
+        self._data[key] = val
+        return key
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def get(self, key, default=None):
+        if key == self.missing_key:
+            return default
+        return self._data.get(key, default=default)
+
+    def values(self):
+        return self._data.values()
