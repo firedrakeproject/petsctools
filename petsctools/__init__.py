39c399a954952eb5714639d1ad62d5e17ad66bcd
Connor Ward <c.ward20@imperial.ac.uk>
2025-09-25 11:09:43 +0100

    Add citations management functions (#14)
    * Add citations management functions
    
    * Add a test

--- petsctools/__init__.py
+++ petsctools/__init__.py
@@ -14,6 +14,7 @@ from .utils import PETSC4PY_INSTALLED
 # is not available then attempting to access these attributes will raise an
 # informative error.
 if PETSC4PY_INSTALLED:
+    from .citation import add_citation, cite, print_citations_at_exit  # noqa: F401
     from .config import get_blas_library  # noqa: F401
     from .init import (  # noqa: F401
         InvalidEnvironmentException,
@@ -37,6 +38,9 @@ else:
 
     def __getattr__(name):
         petsc4py_attrs = {
+            "add_citation",
+            "cite",
+            "print_citations_at_exit",
             "get_blas_library",
             "InvalidEnvironmentException",
             "InvalidPetscVersionException",
